f7a6c399d7fbbbd192ed4846dd2812214f481efa
VisualWhisper94 <codyramsey@mec.science>
2019-07-31 14:11:49 -0400

    MadLib Python

---
+++ Day7/MadLibs/myStory.py
@@ -0,0 +1,22 @@
+#this is my story with spaces thats going to imput the user's input.
+story = """"My mother is {0} because she{1} 
+of me and always make me",{2}, {3}
+.When im feeling", {4},my mom, {5}, me, and when I feel
+, {6},she, {7}, everything feel, {8}.My mom always make sure I
+,{9}, "my homework", {10}, "my teeth, 
+and, {11}, my room. I,{12}, my mom."""
+
+adjective1 = raw_input ("Enter your first adjective:\n")
+verb1 = raw_input("Enter your first verb:\n")
+verb2 = raw_input("Enter your second verb:\n")
+adjective2 = raw_input ("Enter your second adjecttive:\n")
+adjective3 = raw_input()
+verb3 = raw_input
+adjective4 = raw_input
+verb4 = raw_input
+adjective5 = raw_input
+verb5 = raw_input
+verb6 = raw_input
+verb7 = raw_input
+adjective6 = raw_input
+print story.format(adjective1, verb1, verb2, adjective2, adjective3, verb3, adjective4, verb4, adjective5, verb5, verb6, verb7, adjective6)
